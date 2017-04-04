19f3b39ff4b75a66ad3aa385e44b5300e9284fce
RyanKelly27 <rrk27@me.com>
2017-04-04 14:57:02 -0400

    Position framework
    More of the framework for the position class done.

--- Position/src/Position.java
+++ Position/src/Position.java
@@ -1,16 +1,38 @@
 
 public class Position {
 
-	public static int num[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+	private int list[][];
 	
 	public Position()
 	{
-		int list[][] = num;
+		int list[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+		//Repeatedly getting null error.
+		//When establishing a new Position variable, the list array is null, why?
 	}
 	
+	
+	/*
+	 * Checks to see if the next letter selection is valid or not.
+	 * If it is, return true.
+	 * If not, return false.
+	 */
 	public boolean checkIfValid(int a, int b)
 	{
-		if()
+		return false;
+	}
+	
+	
+	//Prints out all the elements in the matrix.
+	public void printMatrix()
+	{
+		for(int i=0; i<4; i++)
+		{
+			for(int a=0; a<4; a++)
+			{
+				System.out.print(list[i][a] +" ");
+			}
+			System.out.println("\n");
+		}
 	}
 	
 	
@@ -18,16 +40,21 @@ public class Position {
 	{
 		Position newList = new Position();
 		
-		for(int i=0; i<4; i++)
-		{
-			for(int a=0; a<4; a++)
-			{
-				System.out.print(newList.[i][a] +" ");
-			}
-			System.out.println("\n");
-		}
+		newList.printMatrix();
 	}
 	
-	
+	/** List of must needs
+	 * 
+	 * 1) The same letter can't be picked twice in one word.
+	 * 	EX. spot 3 picked twice in one word.
+	 * 
+	 * 2) The next letter must be adjacent.
+	 * 	EX. If spot 1 is chosen, spot 16 can't be chosen next.
+	 * 
+	 * 	How to incorporate these needs?
+	 * 
+	 * 1) T
+	 * 
+	 */
 	
 }
