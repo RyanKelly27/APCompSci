64c0501d2ac677c691f6ac62bd18b774cdb76f40
RyanKelly27 <rrk27@me.com>
2017-04-04 14:12:06 -0400

    Position Class For Boggle Game
    Just the start

---
+++ Position/src/Position.java
@@ -0,0 +1,33 @@
+
+public class Position {
+
+	public static int num[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+	
+	public Position()
+	{
+		int list[][] = num;
+	}
+	
+	public boolean checkIfValid(int a, int b)
+	{
+		if()
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Position newList = new Position();
+		
+		for(int i=0; i<4; i++)
+		{
+			for(int a=0; a<4; a++)
+			{
+				System.out.print(newList.[i][a] +" ");
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	
+	
+}
